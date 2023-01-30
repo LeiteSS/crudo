28290bce346e5e37ed3beb0a4794ab884c17d98b
LeiteSS <estoudente.program@gmail.com>
2023-01-30 15:22:39 -0300

    Add new prompts to main/__init__.py and /main/prompts/__init__.py

--- src/app/main/__init__.py
+++ src/app/main/__init__.py
@@ -2,13 +2,41 @@ from prompt_toolkit.styles import Style
 from prompt_toolkit.validation import ValidationError
 from prompt_toolkit.validation import Validator
 
+from main.model.form import Form
+from main.model.form import FormField
+from main.model.form import form
+from main.prompt import prompt
+from main.prompt import unsafe_prompt
+from main.prompts.autocomplete import autocomplete
+from main.prompts.checkbox import checkbox
+from main.prompts.common import Choice
+from main.prompts.common import Separator
+from main.prompts.common import print_formatted_text as print
+from main.prompts.confirm import confirm
+from main.prompts.password import password
+from main.prompts.path import path
+from main.prompts.rawselect import rawselect
+from main.prompts.select import select
 from main.prompts.text import text
-
+from main.model.question import Question
 
 __all__ = [
+  "autocomplete",
+  "checkbox",
+  "confirm",
+  "password",
+  "path",
+  "rawselect",
+  "select",
   "text",
+  "print",
+  "form",
   "prompt",
   "unsafe_prompt",
+  "Form",
+  "FormField",
+  "Question",
+  "Choice",
   "Style",
   "Separator",
   "Validator",
--- src/app/main/prompts/__init__.py
+++ src/app/main/prompts/__init__.py
@@ -1,10 +1,25 @@
+from main.prompts import autocomplete
+from main.prompts import checkbox
+from main.prompts import confirm
+from main.prompts import password
+from main.prompts import path
+from main.prompts import rawselect
+from main.prompts import select
 from main.prompts import text
 
 AVAILABLE_PROMPTS = {
+    "autocomplete": autocomplete.autocomplete,
+    "confirm": confirm.confirm,
     "text": text.text,
-
+    "select": select.select,
+    "rawselect": rawselect.rawselect,
+    "password": password.password,
+    "checkbox": checkbox.checkbox,
+    "path": path.path,
     # backwards compatible names
+    "list": select.select,
     "input": text.text,
+    "rawlist": rawselect.rawselect,
 }
 
 def prompt_by_name(name):
